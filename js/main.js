496d35b7c49d6c61df5ed40118ef3da198928cc2
Lisa-str <Lisa.Staruch@studentfr.ch>
2023-02-01 11:37:11 +0100

    fini

--- js/main.js
+++ js/main.js
@@ -1,6 +1,60 @@
-var changeColor = document.querySelector(".change-color");
+var yellowBox = document.querySelector(".change-color");
+
 var changeColorToBlue = function () {
-  changeColor.classList.add("is-active");
+  yellowBox.classList.add("is-active");
 };
 
-changeColor.addEventListener("click", changeColorToBlue);
+yellowBox.addEventListener("click", changeColorToBlue);
+
+// ----------
+
+var orangeBox = document.querySelector(".toggle-color");
+
+var toggleColorToRed = function () {
+  orangeBox.classList.toggle("target");
+};
+
+orangeBox.addEventListener("click", toggleColorToRed);
+
+// ----------
+
+var violetBox = document.querySelector(".trigger");
+var redBox = document.querySelector(".target");
+
+var hideRedBox = function () {
+  redBox.classList.toggle("is-hidden");
+};
+
+violetBox.addEventListener("click", hideRedBox);
+
+// ----------
+
+var greenBox = document.querySelector(".trigger-all");
+var orangeBox = document.querySelector(".toggle-color");
+
+var changeColorToRed = function () {
+  orangeBox.classList.add("target");
+};
+
+greenBox.addEventListener("click", changeColorToRed);
+greenBox.addEventListener("click", changeColorToBlue);
+
+// ----------
+
+var blueBox = document.querySelector(".trigger-all-self");
+var changeColorToGreen = function () {
+  blueBox.classList.add("green-box");
+};
+blueBox.addEventListener("click", changeColorToGreen);
+
+var orangeBox = document.querySelector(".toggle-color");
+var changeColorToRed = function () {
+  orangeBox.classList.add("target");
+};
+blueBox.addEventListener("click", changeColorToRed);
+
+var yellowBox = document.querySelector(".change-color");
+var changeColorToBlue = function () {
+  yellowBox.classList.add("is-active");
+};
+blueBox.addEventListener("click", changeColorToBlue);
